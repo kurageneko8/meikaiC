6f2e4d5cdb6e967396ec57deb391a66f054fad5d
ryo <ryo@ryonoMacBook-ea.local>
2020-01-11 22:55:07 +0900

    this is my first commit

---
+++ 1/e1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("15 - 37 = %d\a\n", 15 - 37);
+    return 0;
+}
---
+++ 8/List02.c
@@ -0,0 +1,14 @@
+int main(int argc, char const *argv[])
+{
+    int     n;
+    double  x;
+    
+    pritf("input int:");
+    scanf("%d", %n);
+    printf("double number is %d\n", sqr(n));
+    
+    pritf("input real number:");
+    scanf("%lf", %x);
+    printf("double number is %f\n", sqr(x));
+    return 0;
+}
